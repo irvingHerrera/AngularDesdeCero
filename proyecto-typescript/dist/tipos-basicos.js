e25c900e55fc85c2d41744bfca0b4e13dcd4c02d
Irving Herrera <irving.herreramolina@gmail.com>
2018-05-15 15:15:37 -0500

    array y enumerados

--- proyecto-typescript/dist/tipos-basicos.js
+++ proyecto-typescript/dist/tipos-basicos.js
@@ -1,3 +1,35 @@
 "use strict";
 var numeroEstudiantes = 40;
 console.log('numero de estudiantes: ', numeroEstudiantes);
+var cursoIniciado = true;
+var nombreCurso = 'Angular dese Cero';
+var contenidoCurso = "\n    Herramientas de Desarrollo\n    Introduccion a TypeScript\n    Introduccion a Angular\n";
+console.log('Nombre curso:', nombreCurso);
+console.log('Contenido', contenidoCurso);
+var bienvenida = "Bienvenidos al curso " + nombreCurso + " ";
+console.log('Bienvenidos ', bienvenida);
+var arregloA = [1, 2, 3, 4];
+console.log('arregloA:', arregloA);
+var arregloB;
+arregloB = ['a', 'b', 'c'];
+console.log('arregloB:', arregloB);
+// TypeScript Enums
+var DiaCurso;
+(function (DiaCurso) {
+    DiaCurso[DiaCurso["MARTES"] = 0] = "MARTES";
+    DiaCurso[DiaCurso["VIERNES"] = 1] = "VIERNES";
+})(DiaCurso || (DiaCurso = {}));
+;
+var DiasSemana;
+(function (DiasSemana) {
+    DiasSemana[DiasSemana["LUNES"] = 0] = "LUNES";
+    DiasSemana[DiasSemana["MARTES"] = 1] = "MARTES";
+    DiasSemana[DiasSemana["MIERCOLES"] = 2] = "MIERCOLES";
+    DiasSemana[DiasSemana["JUEVES"] = 3] = "JUEVES";
+    DiasSemana[DiasSemana["VIERNES"] = 4] = "VIERNES";
+})(DiasSemana || (DiasSemana = {}));
+;
+var primerDia;
+primerDia = DiaCurso.MARTES;
+console.log('Primer dia:', primerDia);
+console.log('Primer dia (string):', DiaCurso[primerDia]);
--- proyecto-typescript/src/tipos-basicos/tipos-basicos.ts
+++ proyecto-typescript/src/tipos-basicos/tipos-basicos.ts
@@ -1,2 +1,34 @@
 let numeroEstudiantes: number = 40;
 console.log('numero de estudiantes: ', numeroEstudiantes);
+
+let cursoIniciado: boolean = true;
+
+let nombreCurso:string = 'Angular dese Cero';
+let contenidoCurso:string = `
+    Herramientas de Desarrollo
+    Introduccion a TypeScript
+    Introduccion a Angular
+`;
+
+console.log('Nombre curso:', nombreCurso);
+console.log('Contenido', contenidoCurso);
+
+let bienvenida:string = `Bienvenidos al curso ${nombreCurso} `;
+
+console.log('Bienvenidos ', bienvenida);
+
+let arregloA: number[] = [1,2,3,4];
+console.log('arregloA:', arregloA);
+
+let arregloB: Array<string>;
+arregloB = ['a','b','c'];
+console.log('arregloB:', arregloB);
+
+// TypeScript Enums
+enum DiaCurso {MARTES, VIERNES};
+enum DiasSemana {LUNES, MARTES, MIERCOLES, JUEVES, VIERNES};
+
+let primerDia: DiaCurso;
+primerDia = DiaCurso.MARTES;
+console.log('Primer dia:', primerDia);
+console.log('Primer dia (string):', DiaCurso[primerDia]);

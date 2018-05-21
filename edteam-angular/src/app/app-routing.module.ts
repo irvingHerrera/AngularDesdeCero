ebdc70a9d362d4fbccad93ac01cf380c9f429196
Irving Herrera <irving.herreramolina@gmail.com>
2018-05-21 11:51:06 -0500

    rutas basicas

--- edteam-angular/src/app/app-routing.module.ts
+++ edteam-angular/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MaterialDemoComponent } from './material/material-demo/material-demo.component';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'material-demo',
     component: MaterialDemoComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
 

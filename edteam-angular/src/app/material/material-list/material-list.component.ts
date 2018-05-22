69c1f153a8bda019d745d5acba3e1b58b415e212
Irving Herrera <irving.herreramolina@gmail.com>
2018-05-22 10:04:18 -0500

    implementacion de modelo mediante interfaz

--- edteam-angular/src/app/material/material-list/material-list.component.ts
+++ edteam-angular/src/app/material/material-list/material-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Estudiante } from '../../shared/model/estudiante';
 
 @Component({
   selector: 'ih-material-list',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaterialListComponent implements OnInit {
 
+  estudiante1: Estudiante;
+  estudiante2: Estudiante;
+  estudiante3: Estudiante;
+
   constructor() { }
 
   ngOnInit() {
+    this.estudiante1 = {
+      id: 1,
+      nombre: 'Luis Aviles',
+      cuidad: 'Cochabamba',
+      fotoURL: 'https://i2.wp.com/ausdroid.net/wp-content/uploads/2017/05/contacts.png?resize=300%2C300'
+    };
+
+    this.estudiante2 = {
+      id: 2,
+      nombre: 'Jorge Perez',
+      cuidad: 'Lima'
+    };
+      
+    this.estudiante3 = {
+      id: 3,
+      nombre: 'Maria Rodriguez',
+      cuidad: 'Bogota'
+    };
+
   }
 
 }
---
+++ edteam-angular/src/app/shared/model/estudiante.ts
@@ -0,0 +1,6 @@
+export interface Estudiante {
+    id: number;
+    nombre: string;
+    cuidad: string;
+    fotoURL?: string;
+}

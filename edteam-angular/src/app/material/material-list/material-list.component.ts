ec5bd27f520da9947982b7a20802fe87fce5f541
Irving Herrera <irving.herreramolina@gmail.com>
2018-05-22 22:42:50 -0500

    ng if

--- edteam-angular/src/app/material/material-list/material-list.component.ts
+++ edteam-angular/src/app/material/material-list/material-list.component.ts
@@ -13,31 +13,36 @@ export class MaterialListComponent implements OnInit {
   estudiante2: Estudiante;
   estudiante3: Estudiante;
 
+  readonly DEFAULT_PICTURE: string = 'http://cdn.osxdaily.com/wp-content/uploads/2015/08/twitter-ios-icon.jpg';
+
   constructor() { }
 
   ngOnInit() {
     this.estudiante1 = new ClassEstudiante(1,
                                           'Luis Aviles',
                                           'Cochabamba',
-                                          'https://i2.wp.com/ausdroid.net/wp-content/uploads/2017/05/contacts.png?resize=300%2C300');
+                                          );
 
     this.estudiante2 = {
       id: 2,
       nombre: 'Jorge Perez',
-      cuidad: 'Lima'
+      cuidad: 'Lima',
+      fotoURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSc5D4_UK0Vyj_p-yBtJ2dD83GBPTordB9lZKf2nZVhhR0Deoh'
     };
       console.log("edad", this.estudiante1.getEdad());
     this.estudiante3 = {
       id: 3,
       nombre: 'Maria Rodriguez',
-      cuidad: 'Bogota'
+      cuidad: 'Bogota',
+      fotoURL: 'https://orig00.deviantart.net/f5aa/f/2016/199/7/6/profile_picture_by_random_explosion-daag6xh.png'
     };
 
     setTimeout(() => {
       this.estudiante3 = {
         id: 4,
         nombre: 'Irving Herrera',
-        cuidad: 'Tuxtla'
+        cuidad: 'Tuxtla',
+        fotoURL: 'https://orig00.deviantart.net/f5aa/f/2016/199/7/6/profile_picture_by_random_explosion-daag6xh.png'
       }
     }, 3000);
 

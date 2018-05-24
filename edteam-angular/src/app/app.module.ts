08806713b96adb5ad24f206f78da45d3fbfb2e5f
Irving Herrera <irving.herreramolina@gmail.com>
2018-05-24 09:40:17 -0500

    se creo servicio para consumir servicio de nasa

--- edteam-angular/src/app/app.module.ts
+++ edteam-angular/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { HomeComponent } from './home/home.component';
+import { NasaApiService } from './shared/service/nasa-api.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { HomeComponent } from './home/home.component';
     BrowserAnimationsModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [NasaApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
